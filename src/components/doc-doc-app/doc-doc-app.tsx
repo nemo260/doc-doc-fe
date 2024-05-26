1282659e278f4e816fe59844927156179f0a99f4
martin.nemec <nneemmeecc.m@gmail.com>
2024-05-26 10:51:19 +0200

    update

--- src/components/doc-doc-app/doc-doc-app.tsx
+++ src/components/doc-doc-app/doc-doc-app.tsx
@@ -1,20 +1,23 @@
-import { Component, Host, Prop, h } from '@stencil/core';
+import { Component, State, Host, Prop, h } from '@stencil/core';
 
 @Component({
   tag: 'doc-doc-app',
   styleUrl: 'doc-doc-app.css',
   shadow: true,
 })
-export class MyComponent {
+export class DocDocApp {
 
   @Prop() basePath: string="";
+  @State() private relativePath = "";
 
   componentWillLoad() {
     const baseUri = new URL(this.basePath, document.baseURI || "/").pathname;
 
     const toRelative = (path: string) => {
       if (path.startsWith(baseUri)) {
+        this.relativePath = path.slice(baseUri.length)
       } else {
+        this.relativePath = ""
       }
     }
 
@@ -29,6 +32,10 @@ export class MyComponent {
 
   render() {
     let path = window.location.pathname;
+
+    if (this.relativePath) {
+      this.relativePath = this.relativePath;
+    }
 
     const getComponent = () => {
       if (path.startsWith('/edit/')) {
