0a2e60b9abd9a8afaf32d63064f956b92c167ac2
martin.nemec <nneemmeecc.m@gmail.com>
2024-05-26 11:06:24 +0200

    pridanie autora

--- src/components/doc-doc-list/doc-doc-list.tsx
+++ src/components/doc-doc-list/doc-doc-list.tsx
@@ -64,6 +64,7 @@ export class DocDocList {
 
     return (
       <Host class="app-container">
+        <p>Autor: Martin Nemec</p>
         {this.errorMessage ? <div class="error">{this.errorMessage}</div> :
         <md-list>
           <md-list-item>
